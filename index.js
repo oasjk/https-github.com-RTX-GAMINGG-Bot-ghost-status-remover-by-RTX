e8fe79fc49d1d6a2c20c5739fe2605edb8be4ec8
oasjk <oskarschaarschmidt@gmail.com>
2024-08-10 13:19:20 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Playing a game🕵🏻‍♂️ made by @i_amsneha"];
+const statusMessages = ["SkyWings Support - Listening to your requests"];
 
 
 let currentIndex = 0;
